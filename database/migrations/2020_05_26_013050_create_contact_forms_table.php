800e381a20edef4e2964230cc312b9618d539316
github tsubasashimazaki <github>
2020-05-26 17:47:45 +0900

    コンタクトフォーム カラム作成

--- database/migrations/2020_05_26_013050_create_contact_forms_table.php
+++ database/migrations/2020_05_26_013050_create_contact_forms_table.php
@@ -12,9 +12,20 @@ class CreateContactFormsTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   
+        //データベース大文字小文字が確認できない場合があるためスネーク 複数形contact_forms
         Schema::create('contact_forms', function (Blueprint $table) {
             $table->bigIncrements('id');
+            //氏名・メールアドレス・url・性別・年齢・お問い合わせ内容
+            $table->string('your_name', 20);
+            $table->string('email', 255);
+            // カラム修飾子 = カラムにタイプを付属させえたい時 nullable = 空っぽでもOKという意味
+            $table->longText('url')->nullable($value = true);
+            //性別は0か1なのでboolean
+            $table->boolean('gender');
+            //少ない文字tinyInteger
+            $table->tinyInteger('age');
+            $table->string('contact', 200);
             $table->timestamps();
         });
     }
